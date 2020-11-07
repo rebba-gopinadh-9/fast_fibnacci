9eb99b587b0836e623f012a4de87e6d21b998229
rebba-gopinadh-9 <62297562+rebba-gopinadh-9@users.noreply.github.com>
2020-11-07 21:03:34 +0530

    Create N-dig-in-natural-numbers-concat.c

---
+++ N-dig-in-natural-numbers-concat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int dig(int i,int r,int p){
+    if (r==0){
+        return i%10;
+    }else {
+        i=i+1;
+        return (i %((int) pow(10,p)));
+    }
+}
+int main(){
+    int N;
+    printf("Enter the Number:");
+    scanf("%d",&N);
+    int k=1,p=0;
+    while (N>k*9*pow(10,p)){
+        N=N-k*9*pow(10,p);
+        k++;
+        p++;
+    }
+    int q=N/k,r=N%k;
+    int i=(int)pow(10,p)+q;
+    printf("%d\n",dig(i,r,p));
+    return 0;
+}
