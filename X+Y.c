adf72f599898a44ffd029732e6e5070ef4b8bf58
rebba-gopinadh-9 <62297562+rebba-gopinadh-9@users.noreply.github.com>
2020-11-15 01:26:53 +0530

    Update X+Y.c

--- X+Y.c
+++ X+Y.c
@@ -5,8 +5,8 @@ typedef struct {
     int y;
     int sum;
 }point;
-void merge(int A[],int p,int q,int r){
-    int n1=q-p+1,n2=r-q;
+void merge(int A[],int p,int q,int r){             //just to merge the the lines of the matrix so formed.
+    int n1=q-p+1,n2=r-q;                           //changes have to done .
     int L[n1],R[n2];
     for (int i=0;i<n1;i++){
         L[i]=A[p+i];
@@ -34,5 +34,34 @@ void merge(int A[],int p,int q,int r){
         A[k]=L[j];
         j++;
         k++;
+    }
+}
+void swap(int *a,int *b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+void max_heapify(int A[],int n,int i){
+    int left=2*i+1;
+    int right=2*i+2;
+    int large=i;
+    if (A[left]>A[large] && left<n){
+        large=left;
+    }
+    if (A[right]>A[large] && right <n){
+        large=right;
+    }
+    if (large!=i){
+        swap(&A[large],&A[i]);
+        max_heapify(A,n,large);
+    }
+}
+void heap_sort(int A[],int n){                     //heap sort for sorting the X & Y seperately to have O(lg n) time
+     for (int i=n/2-1;i>=0;i--){
+        max_heapify(A,n,i);
+    }
+    for (int i=n-1;i>0;i--){
+        swap(&A[0],&A[i]);
+        max_heapify(A,i,0);
     }
 }
