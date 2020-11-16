808d058bc7df86ab8f09f841161addea7921721e
rebba-gopinadh-9 <62297562+rebba-gopinadh-9@users.noreply.github.com>
2020-11-16 11:37:17 +0530

    Update X+Y.c

--- X+Y.c
+++ X+Y.c
@@ -3,11 +3,10 @@
 typedef struct {
     int x;
     int y;
-    int sum;
 }point;
-void merge(int A[],int p,int q,int r){             //just to merge the the lines of the matrix so formed.
-    int n1=q-p+1,n2=r-q;                           //changes have to done .
-    int L[n1],R[n2];
+void merge(point A[],int p,int q,int r){
+    int n1=q-p+1,n2=r-q;
+    point L[n1],R[n2];
     for (int i=0;i<n1;i++){
         L[i]=A[p+i];
     }
@@ -16,7 +15,7 @@ void merge(int A[],int p,int q,int r){             //just to merge the the lines
     }
     int i=0,j=0,k=p;
     while (i<n1 && j<n2){
-        if (L[i]<R[j]){
+        if ((L[i].x+L[i].y)<(R[j].y+R[j].x)){
             A[k]=L[i];
             i++;
         }else {
@@ -56,12 +55,36 @@ void max_heapify(int A[],int n,int i){
         max_heapify(A,n,large);
     }
 }
-void heap_sort(int A[],int n){                     //heap sort for sorting the X & Y seperately to have O(lg n) time
-     for (int i=n/2-1;i>=0;i--){
+void heap_sort(int A[],int n){
+    for (int i=n/2-1;i>=0;i--){
         max_heapify(A,n,i);
     }
     for (int i=n-1;i>0;i--){
         swap(&A[0],&A[i]);
         max_heapify(A,i,0);
     }
+}
+void Sort(int A[],int B[],int n){
+    heap_sort(A,n);
+    heap_sort(B,n);
+    point S[n*n];
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            S[n*i+j].x=A[i];
+            S[n*i+j].y=B[j];
+        }
+    }
+    for (int i=1;i<n;i++){
+        merge(S,0,i*n-1,(i+1)*n-1);
+    }
+    for (int i=0;i<n*n;i++){
+        printf("%d  : %d %d Sum:%d\n",i,S[i].x,S[i].y,S[i].x+S[i].y);
+    }
+}
+
+int main(){
+    int A[]={1,2,9};
+    int B[]={2,4,6};
+    Sort(A,B,3);
+    return 0;
 }
